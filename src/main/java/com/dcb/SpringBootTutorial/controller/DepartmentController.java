82c6c67d0bd35ab0a01a744095565112e61ba3cf
srjOfficial <suraj2office@duck.com>
2026-01-12 15:40:25 +0530

    Added Logging

--- src/main/java/com/dcb/SpringBootTutorial/controller/DepartmentController.java
+++ src/main/java/com/dcb/SpringBootTutorial/controller/DepartmentController.java
@@ -3,6 +3,8 @@ package com.dcb.SpringBootTutorial.controller;
 import com.dcb.SpringBootTutorial.entity.Department;
 import com.dcb.SpringBootTutorial.service.DepartmentService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,13 +13,16 @@ import java.util.List;
 @RestController
 public class DepartmentController {
 
+    //By Default Spring gives This Slf4j Logger to use, we can add log4j or other if needed
+    private final Logger logger = LoggerFactory.getLogger(DepartmentController.class);
+
     //give me object automatically via DI
     @Autowired
     private  DepartmentService departmentService;
 
     @PostMapping("/saveDepartment")
     public Department saveDepartment( @Valid  @RequestBody Department department){
-
+        logger.info(" Request Received for saveDepartment  {}", department.toString());
         return departmentService.saveDepartment(department);
     }
 
